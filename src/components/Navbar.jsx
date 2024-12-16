0640065b6dab8a9e26e82177efdc9481e7d0c438
Jorge Matos <jisapjisap@gmail.com>
2024-12-16 19:30:13 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className='w-full h-16 md:h-20 flex items-center justify-between'>
+    <div className='w-full h-16 md:h-20 flex items-center justify-between relative z-30'>
       {/* Logo */}
-      <div className='flex items-center gap-4 text-2xl font-bold'>
+      <div className='flex items-center gap-4 text-2xl font-bold z-10'>
         <img 
           src="/logo.png" 
           alt="Logo"
@@ -19,14 +19,19 @@ const Navbar = () => {
       {/* Mobile Menu */}
       <div className='md:hidden'>
         <div 
-          className='cursor-pointer text-2xl'
+          className='cursor-pointer text-2xl absolute top-4 right-4 z-40'
           onClick={() => setOpen((prev) => !prev)}  
         >
           {open ? "X" : "☰"}
         </div>
-        <div className={`
+        {/* <div className={`
           w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e6e6ff] transition-all ease-in-out
           ${open ? "-right-0" : "-right-[100%]"}
+        `}> */}
+        <div
+          className={`
+          w-full h-screen fixed top-0 left-0 flex flex-col items-center justify-center gap-8 font-medium text-lg bg-[#e6e6ff] transition-transform ease-in-out
+          ${open ? "translate-x-0" : "translate-x-full"}
         `}>
           <a href="/">Home</a>
           <a href="/">Trending</a>
