694e255435f4a0344e9a8917496ad242b85d6df6
Jorge Matos <jisapjisap@gmail.com>
2024-12-19 18:18:23 +0100

    add routing with react-router-dom; implement MainLayout and create new route components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { IKImage } from 'imagekitio-react';
 import Image from './Image';
+import { Link, Links } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -9,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='w-full h-16 md:h-20 flex items-center justify-between relative z-30'>
       {/* Logo */}
-      <div className='flex items-center gap-4 text-2xl font-bold z-10'>
+      <Link to="/" className='flex items-center gap-4 text-2xl font-bold z-10'>
         <Image 
           src="logo.png" 
           className='w-8 h-8'
@@ -18,7 +19,7 @@ const Navbar = () => {
           h={32}
         />
         <span>React Blog</span>
-      </div>
+      </Link>
 
       {/* Mobile Menu */}
       <div className='md:hidden'>
@@ -37,29 +38,29 @@ const Navbar = () => {
           w-full h-screen fixed top-0 left-0 flex flex-col items-center justify-center gap-8 font-medium text-lg bg-[#e6e6ff] transition-transform ease-in-out
           ${open ? "translate-x-0" : "translate-x-full"}
         `}>
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="">
             <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
               Login 🖐
             </button>
-          </a>
+          </Link>
         </div>
       </div>
 
       {/* Desktop Menu */}
       <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="">
           <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
             Login 🖐
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { 
+  createBrowserRouter, 
+  RouterProvider, 
+  Route,  
+} from "react-router-dom";
+import Homepage from './routes/Homepage.jsx';
+import Write from './routes/Write.jsx';
+import SinglePostpage from './routes/SinglePostpage.jsx';
+import LoginPage from './routes/LoginPage.jsx';
+import RegisterPage from './routes/RegisterPage.jsx';
+import PostListPage from './routes/PostListPage.jsx';
+import MainLayout from './layouts/MainLayout.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />
+      },
+      {
+        path: "/post",
+        element: <PostListPage />
+      },
+      {
+        path: "/:slug",
+        element: <SinglePostpage />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+      {
+        path: "/login",
+        element: <LoginPage />
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
