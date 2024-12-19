d0bccd555938f499b96144c0a46dd7e74d69f95f
Jorge Matos <jisapjisap@gmail.com>
2024-12-19 18:40:04 +0100

    add Clerk authentication; implement login and registration pages, update Navbar for user state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { IKImage } from 'imagekitio-react';
 import Image from './Image';
 import { Link, Links } from 'react-router-dom';
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
 
@@ -56,11 +57,20 @@ const Navbar = () => {
         <Link to="/">Trending</Link>
         <Link to="/">Most Popular</Link>
         <Link to="/">About</Link>
-        <Link to="">
-          <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
-            Login 🖐
-          </button>
-        </Link>
+        
+        <SignedOut>
+          <Link to="/login">
+            <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
+              Login 🖐
+            </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+
+        
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,13 @@ import LoginPage from './routes/LoginPage.jsx';
 import RegisterPage from './routes/RegisterPage.jsx';
 import PostListPage from './routes/PostListPage.jsx';
 import MainLayout from './layouts/MainLayout.jsx';
+import { ClerkProvider } from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 const router = createBrowserRouter([
   {
@@ -49,6 +55,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
