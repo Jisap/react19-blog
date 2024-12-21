947cf240d7a965d3cb25689a91359a9a8b938cbb
Jorge Matos <jisapjisap@gmail.com>
2024-12-21 17:32:41 +0100

    PostListItem

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import PostListItem from './PostListItem'
 
 const PostList = () => {
   return (
-    <div>PostList</div>
+    <div className='flex flex-col gap-12 mb-8'>
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+    </div>
   )
 }
 
