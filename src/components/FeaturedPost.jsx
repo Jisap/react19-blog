b3d0ea2c5e6fdf6a1ba19834ebd95b3290d080e4
Jorge Matos <jisapjisap@gmail.com>
2024-12-21 17:01:00 +0100

    Featured Posts

--- src/components/FeaturedPost.jsx
+++ src/components/FeaturedPost.jsx
@@ -21,7 +21,30 @@ const FeaturedPost = () => {
       </div>
 
       {/* Others */}
-      <div className='w-full lg:w-1/2 flex flex-col gap-4'></div>
+      <div className='w-full lg:w-1/2 flex flex-col gap-4'>
+        <div className='lg:h-1/3 flex justify-between gap-4'>
+          <Image 
+            src="featured2.jpeg"
+            className="rounded-3xl object-cover w-1/3 aspect-video"
+          />
+          <div className='w-2/3'>
+            <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
+              <h1 className='font-semibold'>02.</h1>
+              <Link className='text-blue-800'>Web Design</Link>
+              <span className='text-gray-500 text-sm'>2 days ago</span>
+            </div>
+            <Link to="/test" className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl'>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit
+            </Link>
+          </div>
+        </div>
+        <div className='lg:h-1/3 flex justify-between gap-4'>
+
+        </div>
+        <div className='lg:h-1/3 flex justify-between gap-4'>
+
+        </div>
+      </div>
     </div>
   )
 }
