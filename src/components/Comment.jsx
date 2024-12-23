c86148c6c3d6aa84208ce503ac7682cd05756e0a
Jorge Matos <jisapjisap@gmail.com>
2024-12-23 17:11:54 +0100

    comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Image from './Image'
 
 const Comment = () => {
   return (
-    <div>Comment</div>
+    <div className='p-4 bg-slate-50 rounded-xl mb-0'>
+      <div className='flex items-center gap-4'>
+        <Image 
+          src="userImg.jpeg"
+          className="w-10 h-10 rounded-full object-cover"
+          w="40"
+        />
+        <span className='font-medium'>John Doe</span>
+        <span className='text-sm text-gray-500'>2 days ago</span>
+      </div>
+      <div className='mt-4'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit aptent auctor, quis nibh interdum. Dui in ornare conubia curabitur imperdiet
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Comment from './Comment'
 
 const Comments = () => {
   return (
@@ -13,6 +14,7 @@ const Comments = () => {
           Send
         </button>
       </div>
+        <Comment />
     </div>
   )
 }
