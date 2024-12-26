08146a09c1e5be637da48e0e8521274e42a6665b
Jorge Matos <jisapjisap@gmail.com>
2024-12-26 16:58:04 +0100

    Write

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { useUser } from '@clerk/clerk-react';
+import 'react-quill-new/dist/quill.snow.css';
+import ReactQuill from 'react-quill-new';
 
 const Write = () => {
+
+  const { isLoaded, isSignedIn} = useUser()
+
+  if(!isLoaded) {
+    return <div>Loading...</div>
+  }
+
+  if(isLoaded && !isSignedIn) {
+    return <div>Please sign in</div>
+  }
+
   return (
-    <div>Write</div>
+    <div>
+      <h1>
+        Create a New Post
+      </h1>
+      <form>
+        <button>Add a cover image</button>
+        <input type="text" placeholder="My Awesome Story" />
+        <div>
+          <label htmlFor="">Choose a category</label>
+          <select name="cat" id="">
+            <option value="general">General</option>
+            <option value="web-design">Web Design</option>
+            <option value="development">Development</option>
+            <option value="databases">Databases</option>
+            <option value="seo">Search Engines</option>
+            <option value="marketing">Marketing</option>
+          </select>
+        </div>
+        <textarea 
+          name="desc" 
+          placeholder="A short description"
+        />
+        <ReactQuill theme="snow" /> 
+        <button>Send</button>
+      </form>
+    </div>
   )
 }
 
