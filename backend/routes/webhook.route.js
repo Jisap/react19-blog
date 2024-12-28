4976fa404ff26b2881141f860a7c0c0d90ed2093
Jorge Matos <jisapjisap@gmail.com>
2024-12-28 19:19:02 +0100

    webhook controllers

--- backend/routes/webhook.route.js
+++ backend/routes/webhook.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { clerkWebhook } from '../controllers/webhook.controller.js';
+
 
 const router = express.Router();
 
-router.post("/clerk", clerkWebhook)
+router.post("/clerk", bodyParser.raw({type: 'application/json'}), clerkWebhook);
 
 
 
