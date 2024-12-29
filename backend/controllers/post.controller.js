ad8f4ac5cd3f603f8985890454917abde71c78c4
Jorge Matos <jisapjisap@gmail.com>
2024-12-29 22:28:27 +0100

    refactor createPost function for improved error handling and code clarity

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,6 +2,7 @@
 import Post from '../models/post.model.js';
 import User from '../models/user.model.js';
 
+
 export const getPosts = async(req, res) => {
   const posts = await Post.find();
   res.status(200).json(posts);
@@ -12,26 +13,52 @@ export const getPost = async(req, res) => {
   res.status(200).json(post);
 }
 
-export const createPost = async(req, res) => {
+// export const createPost = async(req, res) => {
 
-  const clerkUserId = req.auth.userId; 
-  if(!clerkUserId) {
+//   const clerkUserId = req.auth.userId; 
+//   console.log("clerkuserID", clerkUserId);
+//   if(!clerkUserId) {
+//     return res.status(401).json("Not authenticated");
+//   }
+//   const user = await User.findOne({ clerkId: clerkUserId })
+//   if(!user){
+//     return res.status(401).json("User not found");
+//   }
+
+//   const newPost = new Post({
+//     user: user._id,
+//     ...req.body
+//   });
+
+//   const post = await newPost.save();
+//   res.status(201).json(post);
+// }
+
+export const createPost = async (req, res) => {
+  const clerkUserId = req.auth.userId;
+  if (!clerkUserId) {
     return res.status(401).json("Not authenticated");
   }
 
-  const user = await User.findOne({ clerkUserId })
-  if(!user){
-    return res.status(401).json("User not found");
+  try {
+    const user = await User.findOne({ clerkId: clerkUserId });
+    if (!user) {
+      return res.status(401).json("User not found");
+    }
+
+    const newPost = new Post({
+      user: user._id,
+      ...req.body
+    });
+
+    const post = await newPost.save();
+    res.status(201).json(post);
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).json("Internal server error");
   }
+};
 
-  const newPost = new Post({
-    user: user._id,
-    ...req.body
-  });
-
-  const post = await newPost.save();
-  res.status(201).json(post);
-}
 
 export const deletePost = async(req, res) => {
 
