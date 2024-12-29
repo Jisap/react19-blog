ac445d88d0ff8db3d2dfcb4eb7ac51acb1835126
Jorge Matos <jisapjisap@gmail.com>
2024-12-29 17:44:40 +0100

    implement user authentication for post creation and deletion; enhance Navbar with token retrieval

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,6 @@
+
 import Post from '../models/post.model.js';
+import User from '../models/user.model.js';
 
 export const getPosts = async(req, res) => {
   const posts = await Post.find();
@@ -11,12 +13,43 @@ export const getPost = async(req, res) => {
 }
 
 export const createPost = async(req, res) => {
-  const newPost = new Post(req.body);
+
+  const clerkUserId = req.auth.userId; 
+  if(!clerkUserId) {
+    return res.status(401).json("Not authenticated");
+  }
+
+  const user = await User.findOne({ clerkUserId })
+  if(!user){
+    return res.status(401).json("User not found");
+  }
+
+  const newPost = new Post({
+    user: user._id,
+    ...req.body
+  });
+
   const post = await newPost.save();
   res.status(201).json(post);
 }
 
 export const deletePost = async(req, res) => {
-  const post = await Post.findByIdAndDelete(req.params.id);
+
+  const clerkUserId = req.auth.userId;
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated");
+  }
+
+  const user = await User.findOne({ clerkUserId })
+
+  const deletedPost = await Post.findOneAndDelete({
+    _id:req.params.id, 
+    user: user._id
+  });
+
+  if(!deletedPost){
+    return res.status(403).json("You can delete only your own posts");
+  }
+
   res.status(200).json("Post deleted");
 }
--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,27 @@ import userRoute from './routes/user.route.js';
 import postRoute from './routes/post.route.js';
 import commentRoute from './routes/comment.route.js';
 import webHookRoute from './routes/webhook.route.js';
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
 
 const app = express();
 
-app.use(clerkMiddleware());
+// Frontend
+// Cuando el usuario se loguea exitosamente, Clerk genera un JWT
+// Este token contiene información encriptada del usuario y sus permisos
+// Se almacena automáticamente en el navegador(localStorage o cookies)
+// Cuando se hace una petición a la API, se envía el token
+
+// Backend
+// El clerkMiddleware intercepta la petición
+// Extrae el token del header Authorization
+// Verifica la firma del token usando tu CLERK_SECRET_KEY
+// Decodifica la información del usuario
+// Si es válido: añade req.auth con la información del usuario
+// Si no es válido: genera un error 401
+// La información del usuario en el req.auth se usa en las rutas
+
+app.use(clerkMiddleware()); 
 
 app.use("/webhooks", webHookRoute);
 app.use(express.json());
