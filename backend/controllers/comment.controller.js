50889e42f5a623dc647baf58048bba7ea43fcfb2
Jorge Matos <jisapjisap@gmail.com>
2025-01-05 17:26:55 +0100

    feat: enhance comments functionality by adding user association and improving comment submission and deletion processes

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,5 +1,5 @@
 import Comment from "../models/comment.model.js"
-
+import User from "../models/user.model.js"
 
 
 export const getPostComments = async(req, res) => {
@@ -11,9 +11,40 @@ export const getPostComments = async(req, res) => {
 }
 
 export const addComment = async(req, res) => {
+  const clerkUserId = req.auth.userId
+  const postId = req.params.postId // Id del post al que se agrega el comentario
+  if(!clerkUserId) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
 
+  const user = await User.findOne({clerkUserId})
+
+  const newComment = new Comment({
+    ...req.body,
+    user: user._id,
+    post: postId
+  })
+
+  const savedComment = await newComment.save()
+  res.status(201).json(savedComment);
 }
 
 export const deleteComment = async(req, res) => {
+  const clerkUserId = req.auth.clerkUserId
+  const id = req.params.id  // Id del comentario a eliminar
+  if (!clerkUserId) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
 
+  const user = await User.findOne({ clerkUserId })
+
+  const deletedComment = await Comment.findOneAndDelete({ _id: id, user: user._id })
+
+  if(!deletedComment) {
+    return res.status(404).json({ message: "You can delete only your own comments" });
+  }
+
+  res.status(200).json("Comment deleted");
 }
--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useAuth, useUser } from "@clerk/clerk-react";
 import Comment from './Comment'
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const fetchComments = async (postId) => {
   const res = await axios.get(
@@ -11,7 +12,7 @@ const fetchComments = async (postId) => {
   return res.data;
 };
 
-const Comments = ({postId}) => {
+const Comments = ({ postId }) => {
   const { user } = useUser();
   const { getToken } = useAuth();
 
@@ -20,22 +21,56 @@ const Comments = ({postId}) => {
     queryFn: () => fetchComments(postId),
   });
 
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (newComment) => {
+      const token = await getToken();
+      return axios.post(`${import.meta.env.VITE_API_URL}/comments/${postId}`, newComment, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+    },
+    onError: (error) => {
+      console.log(error.message);
+      toast.error(error.response.data);
+    }
+  });
+
   if (isPending) return "loading...";
   if (error) return "Something went wrong!" + error.message;
-  if (!data) return "Comment not found!";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const data = {
+      desc: formData.get('desc'),
+    }  
+    
+    mutation.mutate(data);
+  }
 
   return (
-    <div className="flex flex-col gap-8 lg:w-3/5">
+    <div className="flex flex-col gap-8 lg:w-3/5 mb-12">
       <h1 className='text-xl text-gray-500 underline'>Comments</h1>
-      <div className='flex items-center justify-between gap-8 w-full'>
+      <form 
+        onSubmit={handleSubmit}
+        className='flex items-center justify-between gap-8 w-full'
+      >
         <textarea 
+          name="desc"
           placeholder='Add a comment...'
           className='w-full p-4 rounded-xl'
         />
         <button className='bg-blue-800 px-4 py-3 text-white font-medium rounded-xl'>
           Send
         </button>
-      </div>
+      </form>
 
       {data.map((comment) => (
         <Comment 
