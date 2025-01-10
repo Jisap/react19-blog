e73236a4cc0c7d89c39051663927cd84aacf8247
Jorge Matos <jisapjisap@gmail.com>
2025-01-10 16:09:35 +0100

    feat: add sorting and category filtering functionality to SideMenu

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -1,10 +1,35 @@
- import React from 'react'
+import React from 'react'
 import Search from './Search'
-import { Link } from 'react-router-dom'
- 
- const SideMenu = () => {
-   return (
-     <div className='px-4 h-max sticky top-8'>
+import { Link, useSearchParams } from 'react-router-dom'
+
+const SideMenu = () => {
+
+  // Cuando los parámetros de búsqueda en la URL cambian, React Router detecta este cambio 
+  // y re-renderiza los componentes que dependen de estos parámetros.
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleFilterChange = (e) => {                      // Cuando cambia el valor del radio button de filtro
+    if (searchParams.get("sort") !== e.target.value) {     // comprueba si el valor actual del radio button es distinto al valor del radio button que se ha cambiado
+      setSearchParams({                                    // si es distinto, actualiza los parámetros de búsqueda con el nuevo valor del radio button
+        ...Object.fromEntries(searchParams.entries()),
+        sort: e.target.value,
+      });
+    }
+  };
+
+  const handleCategoryChange = (category) => {              // Cuando se pulsa en un link de categoría
+    if (searchParams.get("cat") !== category) {             // comprueba si el valor de la categoría actual es distinto al valor de la categoría que se ha pulsado
+      setSearchParams({                                     // si es distinto, actualiza los parámetros de búsqueda con el nuevo valor de la categoría
+        ...Object.fromEntries(searchParams.entries()),
+        cat: category,
+      });
+    }
+  };
+
+
+  return (
+    <div className='px-4 h-max sticky top-8'>
       <h1 className='mb-4 text-sm font-medium'>
         Search
       </h1>
@@ -15,41 +40,45 @@ import { Link } from 'react-router-dom'
       </h1>
       <div className='flex flex-col gap-2 text-sm'>
         <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
-           <input 
-            type="radio" 
-            name="sort" 
-            value="newest" 
-            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800' 
+          <input
+            type="radio"
+            name="sort"
+            onChange={handleFilterChange}
+            value="newest"
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
           />
           <span>Newest</span>
         </label>
-         <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
-           <input
-             type="radio"
-             name="sort"
-             value="popular"
-             className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
-           />
-           <span>Most Popular</span>
-         </label>
-         <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
-           <input
-             type="radio"
-             name="sort"
-             value="trending"
-             className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
-           />
-           <span>Trending</span>
-         </label>
-         <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
-           <input
-             type="radio"
-             name="sort"
-             value="oldest"
-             className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
-           />
-           <span>Oldest</span>
-         </label>
+        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type="radio"
+            name="sort"
+            onChange={handleFilterChange}
+            value="popular"
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
+          />
+          <span>Most Popular</span>
+        </label>
+        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type="radio"
+            name="sort"
+            onChange={handleFilterChange}
+            value="trending"
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
+          />
+          <span>Trending</span>
+        </label>
+        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type="radio"
+            name="sort"
+            onChange={handleFilterChange}
+            value="oldest"
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
+          />
+          <span>Oldest</span>
+        </label>
       </div>
 
 
@@ -57,15 +86,15 @@ import { Link } from 'react-router-dom'
         Categories
       </h1>
       <div className='flex flex-col gap-2 text-sm'>
-        <Link className='underline' to="/posts">All</Link>
-        <Link className='underline' to="/posts?cat=web-design">Web Design</Link>
-        <Link className='underline' to="/posts?cat=development">Development</Link>
-        <Link className='underline' to="/posts?cat=databases">Databases</Link>
-        <Link className='underline' to="/posts?cat=seo">Search Engines</Link>
-        <Link className='underline' to="/posts?cat=marketing">Marketing</Link>
+        <Link className='underline' to="/posts" onClick={() => handleCategoryChange("general")}>All</Link>
+        <Link className='underline' to="/posts?cat=web-design" onClick={() => handleCategoryChange("web-design")}>Web Design</Link>
+        <Link className='underline' to="/posts?cat=development" onClick={() => handleCategoryChange("development")}>Development</Link>
+        <Link className='underline' to="/posts?cat=databases" onClick={() => handleCategoryChange("databases")}>Databases</Link>
+        <Link className='underline' to="/posts?cat=seo" onClick={() => handleCategoryChange("seo")}>Search Engines</Link>
+        <Link className='underline' to="/posts?cat=marketing" onClick={() => handleCategoryChange("marketing")}>Marketing</Link>
       </div>
-     </div>
-   )
- }
- 
- export default SideMenu
+    </div>
+  )
+}
+
+export default SideMenu
